b4b66d4c9c9a66eb52cc95ffa3af6531dd70199d
Lucas Latorre <lucaslatorre@gmail.com>
2019-03-21 17:28:23 -0300

    Primeiro Commit

---
+++ src/components/cadastro/Cadastro.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <h1 class='centralizado'>Cadastro</h1>    
+    </div>
+</template>
+
+
+
+
+
+<script>
+
+</script>
+
+
+
+<style>
+    
+    .centralizado {
+        
+        text-align: center
+    }
+
+</style>
+
